182d2d007e6e0d93e90704c25ad63456dfca3dfe
marhatha <puneet.marhatha@gmail.com>
2024-04-05 12:23:09 -0500

    Create step1-utils.py

---
+++ step1-utils.py
@@ -0,0 +1,20 @@
+import json
+
+def get_categories_from_json(json_file):
+    """
+    Extract category names from a COCO format JSON file.
+
+    Parameters:
+    - json_file: Path to the COCO format JSON file.
+
+    Returns:
+    - A list of category names.
+    """
+    with open(json_file, 'r') as f:
+        data = json.load(f)
+    
+    categories = {category['id']: category['name'] for category in data['categories']}
+    # Sorting categories by ID to ensure they are in the correct order
+    sorted_categories = [categories[cat_id] for cat_id in sorted(categories)]
+    
+    return sorted_categories
